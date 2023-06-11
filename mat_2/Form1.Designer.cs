ebf28e20b59dd73ca20665af2ea1f883574365c4
Rostyslav Kushch <56363115+Rastan2132@users.noreply.github.com>
2023-06-12 01:48:22 +0200

    Добавьте файлы проекта.

---
+++ mat_2/Form1.Designer.cs
@@ -0,0 +1,98 @@
+﻿using static System.Net.Mime.MediaTypeNames;
+using System.Windows.Forms;
+using System.Xml.Linq;
+
+namespace mat_2
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            Go_button = new Button();
+            label1 = new Label();
+            cueTextBox2 = new YOUR_NAMESPACE_HERE.CueTextBox();
+            SuspendLayout();
+            // 
+            // Go_button
+            // 
+            Go_button.Location = new Point(450, 280);
+            Go_button.Name = "Go_button";
+            Go_button.Size = new Size(100, 29);
+            Go_button.TabIndex = 1;
+            Go_button.Text = "Go";
+            Go_button.UseVisualStyleBackColor = true;
+            Go_button.Click += Go_button_Click;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(300, 330);
+            label1.MaximumSize = new Size(400, 100);
+            label1.MinimumSize = new Size(400, 200);
+            label1.Name = "label1";
+            label1.Size = new Size(400, 200);
+            label1.TabIndex = 2;
+            label1.Text = " ";
+            label1.Visible = false;
+            // 
+            // cueTextBox2
+            // 
+            cueTextBox2.AutoCompleteMode = AutoCompleteMode.Append;
+            cueTextBox2.Cue = "Enter number";
+            cueTextBox2.Location = new Point(300, 230);
+            cueTextBox2.Name = "cueTextBox2";
+            cueTextBox2.Size = new Size(400, 27);
+            cueTextBox2.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackgroundImage = (System.Drawing.Image)resources.GetObject("$this.BackgroundImage");
+            BackgroundImageLayout = ImageLayout.Stretch;
+            ClientSize = new Size(982, 653);
+            Controls.Add(cueTextBox2);
+            Controls.Add(label1);
+            Controls.Add(Go_button);
+            Icon = (Icon)resources.GetObject("$this.Icon");
+            MaximumSize = new Size(1000, 700);
+            MinimumSize = new Size(1000, 700);
+            Name = "Form1";
+            Text = "Matematyka dyskretna";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+
+        #endregion
+
+        private Button Go_button;
+        private Label label1;
+        private YOUR_NAMESPACE_HERE.CueTextBox cueTextBox2;
+    }
+}
---
+++ mat_2/Form1.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Numerics;
+using System.Windows.Forms;
+
+namespace mat_2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void CalculateZnElements(BigInteger n, Label label1)
+        {
+            List<BigInteger> elements = new List<BigInteger>();
+
+            for (BigInteger i = 1; i < n; i++)
+            {
+                if (GreatestCommonDivisor(i, n) == 1)
+                {
+                    elements.Add(i);
+                }
+
+                if (elements.Count >= 100)
+                {
+                    break;
+                }
+            }
+
+            if (elements.Count == 0)
+            {
+                label1.Text = "Zbiór Z" + n + " nie posiada elementów.";
+            }
+            else
+            {
+                string elementsString = "Elementy zbioru Z" + n + ": ";
+                foreach (BigInteger element in elements)
+                {
+                    elementsString += element.ToString() + " ";
+                }
+
+                label1.Text = elementsString;
+            }
+        }
+
+        private void FindGenerator(BigInteger n, Label label1)
+        {
+            if (IsPrime(n))
+            {
+                List<BigInteger> factors = GetPrimeFactors(n - 1);
+
+                for (BigInteger i = 2; i < n; i++)
+                {
+                    bool isGenerator = true;
+
+                    foreach (BigInteger factor in factors)
+                    {
+                        if (BigInteger.ModPow(i, (n - 1) / factor, n) == 1)
+                        {
+                            isGenerator = false;
+                            break;
+                        }
+                    }
+
+                    if (isGenerator)
+                    {
+                        label1.Text += "Znaleziono generator: " + i.ToString();
+                        return;
+                    }
+                }
+
+                label1.Text = "Nie znaleziono generatora dla zbioru Z" + n;
+            }
+            else
+            {
+                label1.Text = "Zbiór Z" + n + " nie jest zbiorem liczb pierwszych.";
+            }
+        }
+
+        private bool IsPrime(BigInteger n)
+        {
+            if (n <= 1)
+                return false;
+
+            for (BigInteger i = 2; i <= Sqrt(n); i++)
+            {
+                if (n % i == 0)
+                    return false;
+            }
+
+            return true;
+        }
+
+        private List<BigInteger> GetPrimeFactors(BigInteger n)
+        {
+            List<BigInteger> factors = new List<BigInteger>();
+
+            for (BigInteger i = 2; i <= n; i++)
+            {
+                while (n % i == 0)
+                {
+                    factors.Add(i);
+                    n /= i;
+                }
+            }
+
+            return factors;
+        }
+
+        private BigInteger GreatestCommonDivisor(BigInteger a, BigInteger b)
+        {
+            while (b != 0)
+            {
+                BigInteger temp = b;
+                b = a % b;
+                a = temp;
+            }
+            return a;
+        }
+
+        private BigInteger Sqrt(BigInteger n)
+        {
+            if (n == 0)
+                return 0;
+
+            BigInteger x = (n + 1) / 2;
+            BigInteger y = (x + n / x) / 2;
+
+            while (y < x)
+            {
+                x = y;
+                y = (x + n / x) / 2;
+            }
+
+            return x;
+        }
+
+        private void Go_button_Click(object sender, EventArgs e)
+        {
+            label1.Visible = true;
+            BigInteger number;
+            if (BigInteger.TryParse(cueTextBox2.Text, out number))
+            {
+                CalculateZnElements(number, label1);
+                FindGenerator(number, label1);
+            }
+            else
+            {
+                label1.Text = "Nieprawidłowa liczba!";
+            }
+        }
+    }
+}
